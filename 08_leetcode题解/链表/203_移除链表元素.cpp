5d9d3960558e8be9780962afe63a752ed3a6aac0
SHERlocked93 <443033099@qq.com>
2025-05-13 21:01:24 +0800

    增加lc 203

---
+++ 08_leetcode题解/链表/203_移除链表元素.cpp
@@ -0,0 +1,48 @@
+// 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+//
+// 思路：虚拟头节点
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+#include "../../common.h"
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* removeElements(ListNode* head, int val) {
+  auto dummy = ListNode(0, head);
+  auto curr = &dummy;
+
+  while (curr->next != nullptr) {
+    if (curr->next->val == val) {
+      auto oldNode = curr->next;
+      curr->next = curr->next->next;
+      delete oldNode;
+    } else
+      curr = curr->next;
+  }
+
+  return dummy.next;
+}
+
+int main() {
+  auto h7 = ListNode(6);
+  auto h6 = ListNode(5, &h7);
+  auto h5 = ListNode(4, &h6);
+  auto h4 = ListNode(3, &h5);
+  auto h3 = ListNode(6, &h4);
+  auto h2 = ListNode(2, &h3);
+  auto head = ListNode(1, &h2);
+  const auto res = removeElements(&head, 6);
+
+  cout << res->val;
+}
